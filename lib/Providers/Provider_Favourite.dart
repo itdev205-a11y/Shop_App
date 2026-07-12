77617c701badeb455922bbab1f1f331bd777c2c2
Firas Haron <itdev205@gmail.com>
2026-07-12 12:22:15 +0300

    Add files via upload

---
+++ lib/Providers/Provider_Favourite.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import '../Models/ModelProduct.dart';
+
+
+class FavouriteProvider with ChangeNotifier {
+
+  final List<ModelProduct> _favorites = [];
+
+  bool Color =false;
+
+
+  List<ModelProduct> get favorites => _favorites;
+
+
+
+  void toggleFavorite(ModelProduct product) {
+
+    if (_favorites.contains(product)) {
+
+      _favorites.remove(product);
+
+    } else {
+
+      _favorites.add(product);
+
+    }
+
+    notifyListeners();
+
+  }
+
+
+
+  bool isFavorite(ModelProduct product) {
+
+    return _favorites.contains(product);
+
+  }
+
+}
---
+++ lib/Screens/Cart_Screen.dart
@@ -0,0 +1,324 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:store_app/Screens/Checkout.dart';
+import 'package:store_app/Widgets/Widgets_MyCart/Add_Remove_Number.dart';
+
+import '../Providers/CartProvider.dart';
+
+
+class CartScreen extends StatelessWidget {
+  const CartScreen({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    final cart = context.watch<CartProvider>();
+
+
+    return Scaffold(
+
+      backgroundColor: const Color(0xffF7F7F7),
+
+
+      appBar: AppBar(
+
+        backgroundColor: Colors.white,
+
+        elevation: 0,
+
+        centerTitle: true,
+
+        title: const Text(
+          "My Cart",
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+
+        iconTheme: const IconThemeData(
+          color: Colors.black,
+        ),
+
+      ),
+      
+      body: SafeArea(
+
+        child: cart.cartProducts.isEmpty
+
+            ? const Center(
+
+          child: Column(
+
+            mainAxisAlignment:
+            MainAxisAlignment.center,
+
+            children: [
+
+              Icon(
+                Icons.shopping_cart_outlined,
+                size: 90,
+                color: Colors.grey,
+              ),
+              SizedBox(height: 15),
+
+              Text(
+                "Your Cart is Empty",
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        )
+        
+            : Column(
+          children: [
+
+            Expanded(
+
+              child: ListView.builder(
+                padding: const EdgeInsets.all(16),
+                
+                itemCount:
+                cart.cartProducts.length,
+                
+                itemBuilder: (context,index){
+                  
+                  final product =
+                  cart.cartProducts[index];
+                  
+                  return Container(
+                    margin:
+                    const EdgeInsets.only(
+                      bottom: 18,
+                    ),
+                    padding:
+                    const EdgeInsets.all(15),
+                    decoration: BoxDecoration(
+
+                      color: Colors.white,
+                      
+                      borderRadius:
+                      BorderRadius.circular(22),
+                      
+                      boxShadow: [
+
+                        BoxShadow(
+
+                          color:
+                          Colors.black.withOpacity(0.07),
+                          blurRadius: 15,
+                          offset:
+                          const Offset(0,6),
+
+                        ),
+                      ],
+                    ),
+                    
+                    child: Row(
+
+                      children: [
+                        
+                        ClipRRect(
+                          borderRadius:
+                          BorderRadius.circular(18),
+                          
+                          child: Image.network(
+                            product.image,
+                            width: 90,
+                            height: 90,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        
+                        const SizedBox(width: 15),
+                        Expanded(
+
+                          child: Column(
+                            crossAxisAlignment:
+                            CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                product.name,
+                                maxLines: 1,
+                                overflow:
+                                TextOverflow.ellipsis,
+                                style:
+                                const TextStyle(
+                                  fontSize: 17,
+                                  fontWeight:
+                                  FontWeight.bold,
+                                ),
+                              ),
+                              const SizedBox(height: 10),
+                              Text(
+                                "\$${product.price}",
+                                style:
+                                const TextStyle(
+                                  color:
+                                  Colors.green,
+                                  fontSize: 19,
+                                  fontWeight:
+                                  FontWeight.bold,
+                                ),
+                              ),
+                              
+                              const SizedBox(height: 12),
+
+                             AddRemoveNumber(
+                               product: product,
+                             )
+
+                            ],
+                          ),
+                        ),
+                        /////////////////////////////////
+
+                        Container(
+                          width: 42,
+                          height: 42,
+
+                          decoration: BoxDecoration(
+                            color: Colors.red.withOpacity(0.1),
+                            shape: BoxShape.circle,
+
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.08),
+                                blurRadius: 8,
+                                offset: const Offset(0, 3),
+                              ),
+                            ],
+                          ),
+
+                          child: IconButton(
+                            padding: EdgeInsets.zero,
+
+                            onPressed: () {
+                              cart.removeProduct(product);
+                            },
+
+                            icon: const Icon(
+                              Icons.delete_outline,
+                              color: Colors.red,
+                              size: 24,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+            
+            Container(
+              padding:
+              const EdgeInsets.fromLTRB(
+                20,
+                20,
+                20,
+                25,
+              ),
+              
+              decoration:
+              const BoxDecoration(
+                color: Colors.white,
+                borderRadius:
+                BorderRadius.vertical(
+                  top:
+                  Radius.circular(30),
+                ),
+              ),
+              
+              child: Column(
+                children: [
+                  
+                 Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+                    children: [
+
+                      const Text(
+                        "Total",
+                        style: TextStyle(
+                          fontSize: 21,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+
+
+                      Text(
+                        "\$${cart.totalPrice}",
+                        style: const TextStyle(
+                          color: Colors.green,
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+
+                    ],
+                  ),
+                  
+                  const SizedBox(height: 20),
+                  SizedBox(
+                    width:
+                    double.infinity,
+                    
+                    height:
+                    55,
+                    child:
+                    ElevatedButton(
+                      onPressed: () {
+
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => Checkout(),));
+
+                      },
+                      
+                      style:
+                      ElevatedButton.styleFrom(
+
+                        backgroundColor:
+                        Colors.black,
+                        
+                        shape:
+                        RoundedRectangleBorder(
+                          borderRadius:
+                          BorderRadius.circular(18),
+
+                        ),
+                      ),
+                      
+                      child:
+                      const Text(
+
+                        "Checkout",
+
+                        style:
+                        TextStyle(
+
+                          color:
+                          Colors.white,
+
+                          fontSize: 18,
+
+                          fontWeight:
+                          FontWeight.bold,
+
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
